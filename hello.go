5e3f94019e4fad49943649a423e6689b59024fbf
aaa <aa@qq.com>
2021-03-21 01:13:20 +0800

    some_message

---
+++ hello.go
@@ -0,0 +1,1 @@
+package main                                    import "fmt"                                    func main(){                                    fmt.Print("Hello world !\n")                    }
